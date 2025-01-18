4bc2e27d5ce71cef7e4a3c1ad02123f59872594d
eshislay <sajidaimran611984@gmail.com>
2025-01-18 16:37:53 +0500

    initial commit

---
+++ data type.py
@@ -0,0 +1,15 @@
+print("variables and it's datatypes")
+print("4 primary datatypes: 1.integers , 2.floats , 3.strings , 4.booleans")
+name="eshal khan" #string datatype
+age=13 #integer datatype
+weight= 50.1 # float datatype
+is_student=True  #booleans
+print("name:",name)
+print("age:",age)
+print("weight:",weight,"KG")
+print("status as studet:",is_student)
+print("data type of name:",type(name))
+print("data type of age:",type(age))
+print("datatype of weight:",type(weight))
+print("data type is_student:",type(is_student))
+
