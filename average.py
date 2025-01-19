c141032c83bf3d7ec5ec0b68973fdf2365796f3b
eshislay <sajidaimran611984@gmail.com>
2025-01-19 16:47:50 +0500

    initial commit

---
+++ average.py
@@ -0,0 +1,9 @@
+print("average of 5 trees")
+height1=int(input("enter height of 1 tree"))
+height2=int(input("enter height of 2 tree"))
+height3=int(input("enter height of 3 tree"))
+height4=int(input("enter height of 4 tree"))
+height5=int(input("enter height of 5 tree "))
+sum=height1+height2+height3+height4+height5
+average=sum/5
+print("average of 5 trees",average)
