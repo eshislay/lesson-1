f47e71013102b826ca3f842870b289e724c04241
eshislay <sajidaimran611984@gmail.com>
2025-01-25 16:28:17 +0500

    initial commit

---
+++ if statement.py
@@ -0,0 +1,17 @@
+print("conditional statement")
+weather=input("enter weather as rainy / cloudy / sunny ")
+if weather=="rainy":
+    print("wear a raincoat")
+if weather=="cloudy":
+    print("wear a sweater")
+if weather=="sunny":
+    print("wear a hat")
+print("positive,negative or zero")
+number=input("enter number ")
+number=int(number)
+if number==0:
+     print("0 number")
+if number>0:
+    print("positive number")
+if number<0:
+    print("negative number")
