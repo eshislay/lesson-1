ad69eda8bdb95a0e84caaca12be5b2de0f9cb46f
eshislay <sajidaimran611984@gmail.com>
2025-01-26 16:27:28 +0500

    initial commmit

---
+++ if,elif,else.py
@@ -0,0 +1,10 @@
+print ("NUMBER IS POSITIVE,NEGATIVE OR ZERO")
+number=input("enter number:")
+number=int(number)
+if number>0:
+    print(number,"is positive")
+elif number<0:
+    print(number,"is negative")
+else:
+    print(number,"is zero")
+    
