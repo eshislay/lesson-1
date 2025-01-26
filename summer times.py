fadf640857b7c00f740847f623097e4e2c4a23c4
eshislay <sajidaimran611984@gmail.com>
2025-01-26 16:03:11 +0500

    initial commit

---
+++ summer times.py
@@ -0,0 +1,10 @@
+print("conditional statement")
+temperature=input("enter tempreture as hot / cold / normal")
+if temperature=="hot":
+    print ("wear shorts and t shirt")
+if temperature== "cold":
+    print ("wear a jacket")
+if temperature=="normal":
+    print ("wear a cotton dress")
+    
+        
