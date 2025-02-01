8776608917beec83c7fc31ffe27f0c27c912118c
eshislay <sajidaimran611984@gmail.com>
2025-02-01 16:24:59 +0500

    initial commit

---
+++ indentity operator and membership operator .py
@@ -0,0 +1,17 @@
+print("indentity operator")
+a=10
+b=10
+c=b
+print(a is b)
+print(a is c)
+print(a is not b)
+a=10
+b=20
+print(a is b)
+print(a is not b)
+print("membership operator")
+name="eshal khan"
+print("E" in name)
+print("e" not in name)
+print("z" in name)
+print("z" not in name)
