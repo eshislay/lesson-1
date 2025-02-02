77e8a31c74859f479ca35dbbf4aba0fdf16cc737
eshislay <sajidaimran611984@gmail.com>
2025-02-02 16:35:29 +0500

    initial commit

---
+++ mean value.py
@@ -0,0 +1,13 @@
+mean1=38
+wrong_number=36
+correct_number=56
+total_number=40
+#sum of 40 numbers
+sum=mean1*total_number
+print("the sum of 40 numbers:",sum)
+#corrct sum of these numbers
+sum2=sum-wrong_number+correct_number
+print("corrected sum",sum2)
+#the correct mean
+mean2=sum2/total_number
+print(mean2)
