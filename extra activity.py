ad7175304bfb2648de39068af7d1912a417533fe
eshislay <sajidaimran611984@gmail.com>
2025-02-08 16:27:37 +0500

    initial commit

---
+++ extra activity.py
@@ -0,0 +1,9 @@
+print("positive,negative or zero")
+num=int(input("enter number:"))
+if num>0:
+    print(num,"is positive")
+else:
+    if num<0:
+     print(num,"is negative")
+    else:
+       print(num,"is zero")
