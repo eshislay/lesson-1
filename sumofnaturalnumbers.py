069740bf3cbd5927a17b65d4480c64bb53b533e1
eshislay <sajidaimran611984@gmail.com>
2025-02-15 16:34:50 +0500

    initial commit

---
+++ sumofnaturalnumbers.py
@@ -0,0 +1,8 @@
+print("sum of natural numbers")
+n=int(input("enter the number:"))
+i=1 # initialization
+sum=0
+while i<=n: #condition
+    sum=sum+i
+    i+=1 #increament
+print("sum of",n,"natural numbers:",sum)
