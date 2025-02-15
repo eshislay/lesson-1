255b224cfa708372b4c781b0b7846856c354311a
eshislay <sajidaimran611984@gmail.com>
2025-02-15 16:18:30 +0500

    initial commit

---
+++ while loop .py
@@ -0,0 +1,10 @@
+print("print loop")
+i=0 #initialization
+while i<=10:  #condition
+    print(i)
+    i+=1 #increament
+print("print loop")
+i=10 #initialization
+while i>10:  #condition
+    print(i)
+    i-=1 #decreament
