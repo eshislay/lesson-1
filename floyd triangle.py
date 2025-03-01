53f1a3b04fba46e7c8af3defa53af01d60c510d5
eshislay <sajidaimran611984@gmail.com>
2025-03-01 16:33:25 +0500

    initial commit

---
+++ floyd triangle.py
@@ -0,0 +1,8 @@
+print("floyd triangle")
+n=int(input("enter number of rows:"))
+number=1
+for i in range(1,n+1):
+    for j in range (1,i+1):
+      print(number,end=" ")
+      number+=1
+    print()
